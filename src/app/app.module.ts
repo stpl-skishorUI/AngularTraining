cc0084c0ef120cd28a5cdae82a326bd208d10f57
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2021-04-28 16:47:21 +0530

    routing implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
